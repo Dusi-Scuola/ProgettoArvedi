447e109fc4af2e8b7f471b4c0de386e542f8dd6e
Diego Dusi <diego.dusi.2008@gmail.com>
2026-04-23 18:01:58 +0200

    Ho aggiunto le classi vuote su cui lavoreremo.

---
+++ src/main/java/com/arvedi/model/Tecnico.java
@@ -0,0 +1,16 @@
+package com.arvedi.model;
+
+/**
+ * This class is the MODEL in the MVC architecture.
+ *
+ * The Model contains:
+ *  - the data (state)
+ *  - the basic logic that manipulates the data
+ *
+ * IMPORTANT:
+ *  The Model does NOT know anything about the GUI or CLI.
+ *  It is completely independent.
+ */
+public class Tecnico{
+
+}
